a5e7811145f841f10a69b1525ba0c7c7bb148c28
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-10-30 13:02:28 +0530

    file changes

--- MNISTDataBaseFileReader.java
+++ MNISTDataBaseFileReader.java
@@ -2,34 +2,72 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MNISTDataBaseFileReader extends CSVFileREader {
-//This class is custom made for the MNIST database Dataset
-
+    // This class is custom made for the MNIST database Dataset
+    double[] expectedOutputArray;
+    double[] inputArray;
+    double label;
     MNISTDataBaseFileReader(String filename) throws FileNotFoundException {
         super(filename);
-        
+
     }
-   public double[] readLineToDoubleArray() throws IOException{
-       String line=  singleFileReader.readLine();
-       String [] arrayOfStrings = line.split(","); //CSV files are seperate dby Commas
-       return convertStringArrayToDoubleArray(arrayOfStrings);
-   }
-   private double [] convertStringArrayToDoubleArray(String [] arrayOfStrings){
-      double[] arrayOfDouble= new double[arrayOfStrings.length];
-      for(int i = 0; i < arrayOfStrings.length; i++){
-        arrayOfDouble[i] = Double.parseDouble(arrayOfStrings[i]);
-      }
-      return arrayOfDouble;
-   }
+    public double getLabel(){
+        return label;
+    }
+    public void next(){
+        double [] array;
+        try{
+            array = readLineToDoubleArray();
+            label = array[0];
+            expectedOutputArray = generateExpectedOutputArrayFromLabel();
+            inputArray = generateInputFromBigArray(array);
+
+        }
+        catch(IOException ex){}
+    }
+
+    public double[] readLineToDoubleArray() throws IOException {
+        String line = singleFileReader.readLine();
+        String[] arrayOfStrings = line.split(","); // CSV files are seperate dby Commas
+        return convertStringArrayToDoubleArray(arrayOfStrings);
+    }
+
+    private double[] convertStringArrayToDoubleArray(String[] arrayOfStrings) {
+        double[] arrayOfDouble = new double[arrayOfStrings.length];
+        for (int i = 0; i < arrayOfStrings.length; i++) {
+            arrayOfDouble[i] = Double.parseDouble(arrayOfStrings[i]);
+        }
+        return arrayOfDouble;
+    }
 
     public double[] getExpectedOutputArray() {
-        return null;
-        
+        return expectedOutputArray;
+
     }
-    
-    public double[] generateExpectedOutputArrayFromLabel(int label){
-        return null;
-      
+
+    public double[] getInputArray() {
+        return inputArray;
+    }
+
+    public double[] generateExpectedOutputArrayFromLabel() {
+        double[] expectedOutputArray = new double[10];
+        for (int i = 0; i < 10; i++) {
+            if (i == label) {
+                expectedOutputArray[i] = 1;
+            } else {
+                expectedOutputArray[i] = 0;
+            }
+        }
+        // go to next line
+        return expectedOutputArray;
+    }
+
+public double []generateInputFromBigArray(double [] array){
+    //generates the input array from the total array , that is it excludes the first element of the array
+    double [] inputArray = new double[784];
+    for(int i=0; i<=783; i++){
+      inputArray[i] = array[i+1];
+    }
+    return inputArray;
 }
-   
-    
+
 }
--- Main.java
+++ Main.java
@@ -4,31 +4,32 @@ public class Main
 {   
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		int[] layerArray= {2,2,1};
+		int[] layerArray= {784,2,10};
 		LayerManager myLayerManager = new LayerManager(layerArray);
 		//The new method must be outside the train method.
 
 
 		double [] inputLayer = {2,3};
 		double [] expectedLayer = {1};
-
+		double label;
 		//test drive
 		// Training the network with random values.
 		for(int i=0;i<=1;i++){
-			inputLayer[0]=NN.getRandom();
-			inputLayer[1]=NN.getRandom();
-			Trainer.train(myLayerManager, inputLayer, expectedLayer);
+			try{
+				MNISTDataBaseFileReader myMnistDataBaseFileReader = new MNISTDataBaseFileReader("mnist_test.csv");
+				myMnistDataBaseFileReader.next();
+				expectedLayer= myMnistDataBaseFileReader.getExpectedOutputArray();
+				inputLayer= myMnistDataBaseFileReader.getInputArray();
+				Trainer.train(myLayerManager, inputLayer, expectedLayer);
+				label=myMnistDataBaseFileReader.getLabel();
+				
+			}
+			
+				catch(FileNotFoundException ex){}
+			
 		}
-		try{
-		MNISTDataBaseFileReader myMnistDataBaseFileReader = new MNISTDataBaseFileReader("mnist_test.csv");
 		
-		expectedLayer= myMnistDataBaseFileReader.getExpectedOutputArray();
-		inputLayer= myMnistDataBaseFileReader.getInputArray();
-
-		System.out.println(expectedLayer[0]);
-		}
-		catch(FileNotFoundException ex){}
-		
+	
         
 	}
 }
